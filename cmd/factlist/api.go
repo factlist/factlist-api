b76155e47e2702f184f67c2fad3da8bc8a4a42c5
Öner Çiller <onerciller@gmail.com>
2018-02-18 15:09:42 +0300

    commented migration

--- cmd/factlist/api.go
+++ cmd/factlist/api.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 
-	"github.com/factlist/factlist-api/api/model"
 	"github.com/factlist/factlist-api/api/router"
 	"github.com/factlist/factlist-api/db"
 )
@@ -15,16 +14,16 @@ func main() {
 
 	//Migrate DB
 
-	db.GetDB().AutoMigrate(&model.User{})
-	db.GetDB().AutoMigrate(&model.Evidence{})
-	db.GetDB().AutoMigrate(&model.EvidenceFile{})
-	db.GetDB().AutoMigrate(&model.EvidenceLink{})
-	db.GetDB().AutoMigrate(&model.Claim{})
-	db.GetDB().AutoMigrate(&model.Link{})
-	db.GetDB().AutoMigrate(&model.File{})
-	db.GetDB().AutoMigrate(&model.ClaimEvidence{})
-	db.GetDB().AutoMigrate(&model.ClaimFile{})
-	db.GetDB().AutoMigrate(&model.ClaimLink{})
+	// db.GetDB().AutoMigrate(&model.User{})
+	// db.GetDB().AutoMigrate(&model.Evidence{})
+	// db.GetDB().AutoMigrate(&model.EvidenceFile{})
+	// db.GetDB().AutoMigrate(&model.EvidenceLink{})
+	// db.GetDB().AutoMigrate(&model.Claim{})
+	// db.GetDB().AutoMigrate(&model.Link{})
+	// db.GetDB().AutoMigrate(&model.File{})
+	// db.GetDB().AutoMigrate(&model.ClaimEvidence{})
+	// db.GetDB().AutoMigrate(&model.ClaimFile{})
+	// db.GetDB().AutoMigrate(&model.ClaimLink{})
 
 	// //Initialize Router
 	r := router.Init()
