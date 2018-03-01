67c94c23e6561c62ae88c66d9548bcbe26889a79
Enis Behiç Tuysuz <enisbtuysuz@gmail.com>
2018-03-01 16:46:34 +0300

    Modified environment variables

--- factlist/settings/base.py
+++ factlist/settings/base.py
@@ -33,9 +33,9 @@ REST_FRAMEWORK = {
     )
 }
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 AUTHENTICATION_BACKENDS = ['factlist.users.backends.EmailBackend']
