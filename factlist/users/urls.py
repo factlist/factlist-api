45d7de27e89524de35ab5a01d3d0a4fdb12a3faa
Enis Behiç Tuysuz <enisbtuysuz@gmail.com>
2018-05-17 16:34:33 +0300

    Logout endpoint

--- factlist/users/urls.py
+++ factlist/users/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import UserLoginView, UserSignupView, UserMeView
+from .views import UserLoginView, UserSignupView, UserMeView, UserLogoutView
 
 urlpatterns = [
     url(r'^users/login/$', UserLoginView.as_view(), name='login'),
     url(r'^users/register/$', UserSignupView.as_view(), name='signup'),
     url(r'^users/me/$', UserMeView.as_view(), name='me'),
+    url(r'^users/logout/$', UserLogoutView.as_view()),
 ]
--- factlist/users/views.py
+++ factlist/users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 
 from .serializers import UserSignupSerializer, UserMeSerializer, UserAuthSerializer
 
@@ -32,3 +34,12 @@ class UserLoginView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         return Response(UserMeSerializer(user).data)
+
+
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        Token.objects.filter(user=request.user).delete()
+        Token.objects.create(user=request.user)
+        return Response({'message': 'User logged out successfully'})
