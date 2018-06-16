b303d849d0722ea5e85e86ae9877165ca1550d7e
Enis Behiç Tuysuz <enisbtuysuz@gmail.com>
2018-06-17 00:06:08 +0300

    Added REDIS_HOST to settings

--- factlist/settings/dev.py
+++ factlist/settings/dev.py
@@ -6,7 +6,7 @@ DEBUG = True
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis/1",
+        "LOCATION": "redis://" + os.environ.get("REDIS_HOST") + "/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,
