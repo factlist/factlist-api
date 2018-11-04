49ed84442d16662decdf8f2793dde5d4ee75dd77
Öner Çiller <onerciller@gmail.com>
2018-11-04 19:46:46 +0300

    Add seeders and fakers

---
+++ seeders/20181104155809-link-tags.js
@@ -0,0 +1,22 @@
+const faker = require('faker');
+const _ = require('lodash');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'link_tags',
+      _.times(5, index => {
+        return {
+          link_id: index + 1,
+          topic_id: index + 1,
+          created_at: new Date(),
+          updated_at: new Date()
+        };
+      })
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('link_tags', null, {});
+  }
+};
