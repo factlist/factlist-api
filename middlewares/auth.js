523fd15cb75dfb37242ef26fe9ce9d164b41fd6e
Öner Çiller <onerciller@gmail.com>
2018-11-17 21:57:54 +0300

    Add logger for auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
+const logger = require('../utils/logger');
 module.exports = async (req, res, next) => {
   const token = req.headers['authorization'];
   if (token && token !== 'null') {
     try {
       const authUser = await jwt.verify(token, config.auth.jwtSecret);
       req.authUser = authUser;
-    } catch (e) {
-      console.log(e);
+    } catch (error) {
+      logger.error(error)
     }
   }
   next();
