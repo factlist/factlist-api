691adbd6d609ef249359bafc714fe09ef4133549
Öner Çiller <onerciller@gmail.com>
2018-11-18 16:08:04 +0300

    Add standartize code styling

--- migrations/20181104115948-create-topics.js
+++ migrations/20181104115948-create-topics.js
@@ -1,34 +1,34 @@
 module.exports = {
-	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable("topics", {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER
-			},
-			title: {
-				type: Sequelize.STRING
-			},
-			user_id: {
-				type: Sequelize.INTEGER
-			},
-			created_at: {
-				allowNull: false,
-				type: Sequelize.DATE
-			},
-			updated_at: {
-				allowNull: false,
-				type: Sequelize.DATE
-			},
-			deleted_at: {
-				allowNull: true,
-				type: Sequelize.DATE,
-				field: "deleted_at"
-			}
-		})
-	},
-	down: queryInterface => {
-		return queryInterface.dropTable("topics")
-	}
-}
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('topics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+        field: 'deleted_at'
+      }
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('topics');
+  }
+};
