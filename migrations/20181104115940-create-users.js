9f0cb476431d32159fd22747387f39f391952841
Öner Çiller <onerciller@gmail.com>
2018-11-18 16:09:35 +0300

    Add standartize code styling

--- migrations/20181104115940-create-users.js
+++ migrations/20181104115940-create-users.js
@@ -1,40 +1,40 @@
 module.exports = {
-	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable("users", {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER
-			},
-			name: {
-				type: Sequelize.STRING
-			},
-			username: {
-				type: Sequelize.STRING
-			},
-			email: {
-				type: Sequelize.STRING
-			},
-			password: {
-				type: Sequelize.STRING
-			},
-			created_at: {
-				allowNull: false,
-				type: Sequelize.DATE
-			},
-			updated_at: {
-				allowNull: false,
-				type: Sequelize.DATE
-			},
-			deleted_at: {
-				allowNull: true,
-				type: Sequelize.DATE,
-				field: "deleted_at"
-			}
-		})
-	},
-	down: (queryInterface, Sequelize) => {
-		return queryInterface.dropTable("users")
-	}
-}
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+        field: 'deleted_at'
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
