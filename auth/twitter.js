6d8fc086639cf6ff6d30a812ebf421c8967a35d0
Öner Çiller <onerciller@gmail.com>
2018-11-26 19:48:38 +0300

    Refactor twitter auth

--- auth/twitter.js
+++ auth/twitter.js
@@ -1,10 +1,26 @@
 const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const config = require('../config');
+
+const twitterOptions = { ...config.auth.twitter, passReqToCallback: true };
+
+passport.use(
+  new TwitterStrategy(
+    twitterOptions,
+    async (token, tokenSecret, profile, done) => {
+      User.findOrCreate({ twitter: profile.id }, (err, user) => {
+        return done(err, user);
+      });
+    }
+  )
+);
+
 module.exports = app => {
-  app.get('/twitter', passport.authenticate('twitter'));
+  app.get('/auth/twitter', passport.authenticate('twitter'));
   app.get(
     '/auth/twitter/callback',
     passport.authenticate('twitter', {
-      successRedirect: '/profile',
+      successRedirect: '/test',
       failureRedirect: '/'
     })
   );
