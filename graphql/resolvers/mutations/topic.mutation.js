38743156e3e8e3447384d64d2ead4e215e7dbd71
Öner Çiller <onerciller@gmail.com>
2018-11-28 21:27:17 +0300

    Add user can crud belongs to their account

--- graphql/resolvers/mutations/topic.mutation.js
+++ graphql/resolvers/mutations/topic.mutation.js
@@ -27,7 +27,10 @@ module.exports = {
   updateTopic: async (_, { data: { id, title } }, { db, authUser }) => {
     try {
       check.Auth(authUser);
-      return await db.topics.update({ title: title }, { where: { id: id } });
+      return await db.topics.update(
+        { title: title },
+        { where: { id: id, user_id: authUser.sub } }
+      );
     } catch (error) {
       throw new Error(error);
     }
