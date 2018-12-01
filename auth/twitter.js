881568b336ea2d849c846ae5a8c3a3addf907b39
Öner Çiller <onerciller@gmail.com>
2018-12-02 00:16:24 +0300

    Fix twitter auth

--- auth/twitter.js
+++ auth/twitter.js
@@ -1,3 +1,4 @@
+const User = require('../models').users;
 const passport = require('passport');
 const TwitterStrategy = require('passport-twitter').Strategy;
 const config = require('../config');
@@ -7,21 +8,25 @@ const twitterOptions = { ...config.auth.twitter, passReqToCallback: true };
 passport.use(
   new TwitterStrategy(
     twitterOptions,
-    async (token, tokenSecret, profile, done) => {
-      User.findOrCreate({ twitter: profile.id }, (err, user) => {
-        return done(err, user);
+    async (req, token, tokenSecret, profile, done) => {
+      const user = await User.findOrCreate({
+        where: { twitter: profile.id, username: profile.screen_name }
       });
+      done(null, user);
     }
   )
 );
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 module.exports = app => {
   app.get('/auth/twitter', passport.authenticate('twitter'));
   app.get(
     '/auth/twitter/callback',
-    passport.authenticate('twitter', {
-      successRedirect: '/test',
-      failureRedirect: '/'
-    })
+    passport.authenticate('twitter', { failureRedirect: '/login' }),
+    function(req, res) {
+      res.redirect('/');
+    }
   );
 };
