00afd491a711724091d1e959891f3ef9ee7e74d2
Öner Çiller <onerciller@gmail.com>
2018-12-15 19:09:54 +0300

    Refactor helper functions

---
+++ helpers/get-user-from-tag.js
@@ -0,0 +1,7 @@
+module.exports = getUserFromTag = async tagId => {
+  const tag = await db.tags.findByPk(tagId);
+  const links = await tag.getLinks();
+  const topics = await Promise.all(links.map(link => link.getTopic()));
+  const user = await topics[0].getUser();
+  return user;
+};
