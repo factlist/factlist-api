6a928c1f47f6b2734adc54177e981fbed411ce11
Öner Çiller <onerciller@gmail.com>
2019-01-06 19:07:34 +0300

    Integrate embedly service

---
+++ services/embedly.js
@@ -0,0 +1,11 @@
+const config = require('../config');
+const request = require('axios');
+
+const getPreviewLink = async url => {
+  const api = config.service.embedly.api;
+	const key = config.service.embedly.key;
+ 	const response = await request.get(`${api}?key=${key}&url=${url}`);
+	return response.data;
+};
+
+module.exports = getPreviewLink;
