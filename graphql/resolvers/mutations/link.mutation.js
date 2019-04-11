72c8d6e28acfb5a46206618bf5601eb15edb104f
serafettin <serafettin@serafettin.net>
2019-04-11 20:03:19 +0300

    CreateLink, UpdateLinkTitle and RemoveLink refactored

--- graphql/resolvers/mutations/link.mutation.js
+++ graphql/resolvers/mutations/link.mutation.js
@@ -11,29 +11,33 @@ module.exports = {
       check.Auth(authUser);
       const topic = await db.topics.findByPk(topic_id);
       if (authUser.id === topic.user_id) {
-        return await db.links.create(
+        return  await db.links.create(
           { title, url, topic_id, tags },
           {
             include: [{ model: db.topics }, { model: db.tags }]
           }
 				);
-				link.topic = topic.get({ plain: true });
-				return link;
       }
       throw new Error(config.locale.auth.not_authorized);
     } catch (error) {
       throw new Error(error);
     }
   },
-  updateLink: async (_, { data: { id, title, url } }, { db, authUser }) => {
+  updateLinkTitle: async (_, { data: { id, title } }, { db, authUser }) => {
     try {
       check.Auth(authUser);
       const user = await getUserFromLink(id);
       if (authUser.id === user.id) {
-        return await db.links.update(
-          { title: title, url: url},
-          { where: { id: id } }
-        );
+
+        const link = await db.links.findOne({
+          where : { id: id }
+        });
+        await link.update({
+          title : title
+        });
+  
+        link.user = authUser.get({plain:true});
+        return link;
       }
       throw new Error(config.locale.auth.not_authorized);
     } catch (error) {
